e7976f54f11087c0d40d5d26b28b7edf5237ac8a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 03:06:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
   array = string.split
   new_array=[]
   array.collect do |word|
-    new_array.push(word.reverse)
+    #new_array.push(word.reverse)    USE THESE COMMENTED LINES FOR .each METHOD
+    word.reverse
   end
   #new_array.join(" ")
 end
