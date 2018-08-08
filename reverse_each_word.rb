780219fa3efc3805ebb0b45be662dfd4fbefb66b
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 03:04:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   array = string.split
   new_array=[]
   array.each do |word|
-    new_array.push(word.reverse )
+    new_array.push(word.reverse)
   end
   new_array.join(" ")
 end
