6012fd927d216971e4ebeb39265bd5c77694ad02
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 03:14:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,12 @@
-require 'pry'
-
+string ="Hello there, and how are you?"
 def reverse_each_word(string)
   array = string.split
   #new_array=[]
   array.collect do |word|
     #new_array.push(word.reverse)    USE THESE COMMENTED LINES FOR .each METHOD
-    word.reverse
+    puts word.reverse
   end
   array.join(" ")
   #new_array.join(" ")
-  binding.pry
-
 end
-#reverse_each_word(string)
+reverse_each_word(string)
